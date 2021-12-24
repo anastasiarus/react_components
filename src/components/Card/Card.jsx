afa5f84a690a5f84fa3128d5a3439849a984a436
anastasiarus <81356013+anastasiarus@users.noreply.github.com>
2021-12-24 13:58:41 +0300

    Add files via upload

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,11 +5,12 @@ import classes from "./Card.module.css"
 
 const Card = (props) => {
 
-    return <div className={classes.card} key={props.id} >
+    return  <div className={classes.card} key={props.id} >
         <h3  className={classes.main}>{props.title}</h3>
         <p className={classes.desc}>{props.text}</p>
         <img className={classes.img} src={props.img} alt="card" />
     </div>
+    
 }
 
 export default Card
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,18 +1,48 @@
 import React from "react";
-import classes from "./Content.module.css"
+import classes from "./Content.module.css";
 import Button from "../Button/Button.jsx";
 import Card from "../Card/Card.jsx";
 
-
 const Content = () => {
-    return <div className={classes.content}>
-      <Button className={classes.btn}/> 
-      <Card id={1} title='РОЗА' text='Ро́за — собирательное название видов и сортов представителей рода Шиповник ' img='https://idrawing.ru/images/articles/070-rose2/rose.jpg' />
-      <Card id={2} title='ТЮЛЬПАН' text='Тюльпа́н — род многолетних травянистых луковичных растений семейства Лилейные' img='https://i.pinimg.com/originals/c3/8d/96/c38d968708ddc315827e70fa920b7578.png' />
-      <Card id={3} title='ЛАНДЫШ' text='Ла́ндыш — монотипный либо олиготипный род однодольных растений семейства Спаржевые' img='https://printonic.ru/uploads/images/2016/03/26/img_56f6e72c0c70a.jpg' />
-      <Card id={4} title='ОДУВАНЧИК' text='Одува́нчик — род многолетних травянистых растений семейства Астровые, или Сложноцветные' img='https://kakrisovat.top/wp-content/uploads/2019/01/dandelion-10.png' />
-      <Card id={5} title='ЛАВАНДА' text='Лава́нда — род растений семейства яснотковых' img='https://cdn.pixabay.com/photo/2018/10/29/09/50/lavender-bunch-3780750_1280.png' />
-  </div>
-}
+  return (
+    <div className={classes.content}>
+      <div className={classes.btnWrapper}>
+        <Button className={classes.btn} />
+      </div>
+      <div className={classes.cards}>
+        <Card
+          id={1}
+          title="Kit-kat"
+          text="Хочешь сделать перерыв - съешь Kit-kat"
+          img="https://static.tildacdn.com/tild3661-6139-4463-b837-656362313364/0083e4ddd55cae9f785b.png"
+        />
+        <Card
+          id={2}
+          title="Snikers"
+          text="Не тормози — сникерсни!"
+          img="https://русский-разгуляйка.рф/i/t/8acce4329db81cae71b54f6fdb28c4e6-3.jpg"
+        />
+        <Card
+          id={3}
+          title="Bounty"
+          text="Bounty - райское наслаждение!"
+          img="https://avatars.mds.yandex.net/i?id=6749491b00ce50bf83c324604025213e-4602005-images-thumbs&n=13"
+        />
+        <Card
+          id={4}
+          title="Mars"
+          text="Mars. Все будет в шоколаде!"
+          img="https://i.ebayimg.com/00/s/NjQwWDg1Mw==/z/nOEAAOSwDFBaIUVf/$_57.JPG?set_id=8800005007"
+        />
+        <Card
+          id={5}
+          title="Twix"
+          text="Попробуй обе и реши на чьей стороне ты!"
+          img="https://veralline.com/uploads/images/comparison/2016/04/26/0efeee0799.jpg"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Content
+export default Content;
